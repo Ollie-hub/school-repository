6cf2090df578fd670df1a4b374bffc325b351258
Ollie-hub <Oliv585p@elev.techcollege.dk>
2020-11-26 11:01:03 +0100

    .

--- Snippets/17goals/js/script.js
+++ Snippets/17goals/js/script.js
@@ -1,1 +1,13 @@
-let goalsimg = [""]
+async function fetchData() {
+    let response = await fetch('https://api.mediehuset.net/sdg/goals');
+    let data = await response.json(); 
+
+    return data.result;
+}; 
+
+goals = () => {
+
+    goalsHTML += 
+    `
+    `
+}
